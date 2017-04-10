49844a7a3e412c807bc8abeaf012e4abc00749f1
Jesper Poder <jesper.poder@live.dk>
2017-04-10 12:00:50 +0200

    Ludo game from gitlab instead - are able to run several games

--- ludo_cpp_fra_gitlab/ludo/main.cpp
+++ ludo_cpp_fra_gitlab/ludo/main.cpp
@@ -17,7 +17,19 @@ int main(int argc, char *argv[]){
     ludo_player_random p3, p4;
 
     game g;
-    g.setGameDelay(0); //if you want to see the game, set a delay
+    g.setGameDelay(000); //if you want to see the game, set a delay
+
+    // Add a GUI <-- remove the '/' to uncomment block
+    Dialog w;
+    QObject::connect(&g,SIGNAL(update_graphics(std::vector<int>)),&w,SLOT(update_graphics(std::vector<int>)));
+    QObject::connect(&g,SIGNAL(set_color(int)),                   &w,SLOT(get_color(int)));
+    QObject::connect(&g,SIGNAL(set_dice_result(int)),             &w,SLOT(get_dice_result(int)));
+    QObject::connect(&g,SIGNAL(declare_winner(int)),              &w,SLOT(get_winner()));
+    QObject::connect(&g,SIGNAL(close()),&a,SLOT(quit()));
+    w.show();
+    // //Or don't add the GUI
+    //QObject::connect(&g,SIGNAL(close()),&a,SLOT(quit()));
+    //*/
 
     //set up for each player
     QObject::connect(&g, SIGNAL(player1_start(positions_and_dice)),&p1,SLOT(start_turn(positions_and_dice)));
@@ -40,24 +52,10 @@ int main(int argc, char *argv[]){
     QObject::connect(&g, SIGNAL(player4_end(std::vector<int>)),    &p4,SLOT(post_game_analysis(std::vector<int>)));
     QObject::connect(&p4,SIGNAL(turn_complete(bool)),              &g, SLOT(turnComplete(bool)));
 
-     //add a GUI
-    /*Dialog w;
-    QObject::connect(&g,SIGNAL(update_graphics(std::vector<int>)),&w,SLOT(update_graphics(std::vector<int>)));
-    QObject::connect(&g,SIGNAL(set_color(int)),                   &w,SLOT(get_color(int)));
-    QObject::connect(&g,SIGNAL(set_dice_result(int)),             &w,SLOT(get_dice_result(int)));
-    QObject::connect(&g,SIGNAL(declare_winner(int)),              &w,SLOT(get_winner(int)));
-    //w.show();*/
-    
-    QObject::connect(&g,SIGNAL(close()),&a,SLOT(quit()));
-	
-
-
     for(int i = 0; i < 10000; ++i){
         g.start();
         a.exec();
         g.reset();
     }
-
-
     return 0;
 }
